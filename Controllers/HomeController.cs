c94037d1313277490f49c9bba7d0f68d2dd8b099
mickaël andrieu <andrieu.travail@gmail.com>
2012-12-27 01:32:38 +0100

    tryin a new db configuration

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,8 +15,7 @@ namespace Todo.Site.Controllers
 {
     public class HomeController : Controller
     {
-        public static Dictionary<string, Email> Mails = new  Dictionary<string,Email>();
-        
+
         public ActionResult Index()
         {
             ViewBag.Message = "Application ASP.NET MVC 4 couplé au framework CSS MetroUI";
@@ -36,57 +35,6 @@ namespace Todo.Site.Controllers
             ViewBag.Message = "Ceci est une page de contact.";
 
             return View();
-        }
-
-
-        public ActionResult Confirmation()
-        {
-            Email current = new Email();
-            if (TempData.ContainsKey("confirm"))
-            {
-                current = (Email)TempData["confirm"];
-                TempData.Remove("confirm");
-            }
-           
-            return View();
-        }
-
-
-        // GET: /Clients/
-        public ActionResult InsertClients()
-        {
-            return View();
-        }
-
-        // POST: /Clients/ 
-        [HttpPost]
-        public ActionResult InsertClients(Email contact)
-        {
-            if (ModelState.IsValid)
-            {
-                if (!Mails.ContainsKey(contact.Mail))
-                {
-                    Mails.Add(contact.Mail, contact);
-                }
-                return View("Confirmation", contact);
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Admin()
-        {
-            ViewBag.Message = "Modifiez ce modèle pour dynamiser votre applicaton";
-
-            return View(Mails.Values);
-        }
-
-        /* implémentation d'une vue partielle */
-        [ChildActionOnly]
-        public ActionResult SayHello()
-        {
-            var items = new[] { "World", "toto", "tata" };
-            return PartialView("_SayHello", items);
         }
     }
 }
