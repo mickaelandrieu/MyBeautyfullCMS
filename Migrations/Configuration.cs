e614719e9588d9362e0a6c924bfb5b0a707db069
mickaël andrieu <andrieu.travail@gmail.com>
2013-01-19 21:48:02 +0100

    a test for appHarbor 2

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -10,7 +10,6 @@ namespace Todo.Site.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Todo.Site.Models.BlogContext context)
--- Models/PageModels.cs
+++ Models/PageModels.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +10,7 @@ using System.Data.Entity;
 
 namespace Todo.Site.Models
 {
+    [Table("Page")]
     public class Page
     {
 
