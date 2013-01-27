1884857686e861e8bc97e14751acd32516c69f1d
mickaël andrieu <andrieu.travail@gmail.com>
2013-01-27 03:40:31 +0100

    following appharbor tutorial

---
+++ Models/BlogContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Globalization;
+using System.Web.Security;
+
+namespace Todo.Site.Models
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext()
+            : base("Models_")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<StaticPage> StaticPages { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlogContext, DbMigrationsConfiguration<Todo.Site.Models.BlogContext>>());
+        }
+
+    }
+}
