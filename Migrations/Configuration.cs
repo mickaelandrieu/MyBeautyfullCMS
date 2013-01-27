d3b3ad51a7109c9167a3ea99c7f2810966df03d9
mickaël andrieu <andrieu.travail@gmail.com>
2013-01-27 03:31:37 +0100

    following appharbor tutorial

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -15,20 +15,5 @@ namespace Todo.Site.Migrations
             AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(Todo.Site.Models.BlogContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
     }
 }
