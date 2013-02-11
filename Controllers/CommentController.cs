8ed169e2dca0af55e9c3c81f874bd05f2f2ce5cf
mickaël andrieu <andrieu.travail@gmail.com>
2013-02-11 03:07:24 +0100

    merge

---
+++ Controllers/CommentController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebMatrix.WebData;
+using Todo.Site.Models;
+using System.Data;
+using System.Data.Entity;
+
+namespace Todo.Site.Controllers
+{
+    public class CommentController : Controller
+    {
+        private BlogContext db = new BlogContext();
+
+        //
+        // GET: /Comment/
+
+        public ActionResult Index()
+        {
+            var comments = db.Comments.Include(c => c.User);
+            return View(comments.ToList());
+        }
+
+        //
+        // GET: /Comment/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        //
+        // GET: /Comment/Create
+
+        public ActionResult Create(int ArticleId)
+        {
+            ViewBag.UserId = WebSecurity.GetUserId(User.Identity.Name);
+            ViewBag.ArticleId = ArticleId;
+            return View();
+        }
+
+        //
+        // POST: /Comment/Create
+
+        [HttpPost]
+        public ActionResult Create(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(comment);
+        }
+
+        //
+        // GET: /Comment/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", comment.UserId);
+            return View(comment);
+        }
+
+        //
+        // POST: /Comment/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", comment.UserId);
+            return View(comment);
+        }
+
+        //
+        // GET: /Comment/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        //
+        // POST: /Comment/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/CommentModels.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Todo.Site.Models
+{
+    [Table("Comment")]
+    public class Comment
+    {
+
+        public Comment()
+        { 
+            this.createdAt = DateTime.Now;
+        }
+
+        [Required(ErrorMessage = "Merci de remplir le formulaire")]
+        [Key]
+        public int CommentId { get; set; }
+        [Display(Name = "Date de création")]
+        public DateTime createdAt { get; set; }
+
+        [Required]
+        [Display(Name = "Valider le commentaire ?")]
+        public Boolean status { get; set; }
+
+        [Required]
+        [Display(Name = "Votre commentaire")]
+        public String content { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public UserProfile User { get; set; }
+
+    }
+}
