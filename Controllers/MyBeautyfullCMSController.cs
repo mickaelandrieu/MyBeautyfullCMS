b8fc76ef33a0c37b05a404b60e57bd652d1f3a10
mickaël andrieu <andrieu.travail@gmail.com>
2013-02-24 19:34:40 +0100

    ajouts pages statiques

---
+++ Controllers/MyBeautyfullCMSController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Todo.Site.Controllers
+{
+    public class MyBeautyfullCMSController : Controller
+    {
+        //
+        // GET: /MyBeautyfullCMS/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
